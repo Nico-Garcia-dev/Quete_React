c8d9f85ca82cf4499156dd8a06e89da8e9af28e0
NICO GARCIA <07nico.garcia@gmail.com>
2025-03-31 15:45:45 +0200

    React Basic quete 2

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+	return (
+		<figure className="pokemon-card">
+			<img
+				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				alt="Bulbasaur"
+			/>
+			<figcaption>
+				<h2>Bulbasaur</h2>
+			</figcaption>
+		</figure>
+	);
+}
+
+export default PokemonCard;
